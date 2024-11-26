33895adcd002a4523fc2fba64772404d29010a55
shahob-atm <shahobiddinatamurodov@gmail.com>
2024-11-26 23:18:45 +0500

    order page va order slice lar qo'shildi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 import {Route, Routes} from "react-router-dom";
 import Order from "./Order.jsx";
+import Admin from "./Admin.jsx";
 
 function App() {
 
-  return (
-    <>
-      <Routes>
-        <Route path={"/order"} element={<Order />} />
-      </Routes>
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path={"/order"} element={<Order />} />
+                <Route path={"/admin"} element={<Admin />} />
+            </Routes>
+        </>
+    )
 }
 
 export default App;
