0ab56eb41ae0039960538e8e1fe885d76a3c023f
shahob-atm <shahobiddinatamurodov@gmail.com>
2024-11-26 23:33:32 +0500

    order page va order slice lar qo'shildi

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit";
 import foodReducer from "./slices/foodSlice";
 import orderReducer from "./slices/orderSlice";
+import adminReducer from "./slices/adminSlice";
 
 export const store = configureStore({
     reducer: {
         foods: foodReducer,
         orders: orderReducer,
+        admin: adminReducer
     }
 });
 
