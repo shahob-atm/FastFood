b10495c803b8411cba7ac60eadbeca68451ffbf2
shahob-atm <shahobiddinatamurodov@gmail.com>
2024-11-28 01:33:32 +0500

    order qilish ga o'zgarish kiritildi

--- backend/src/main/java/org/example/backend/service/order/OrderServiceImpl.java
+++ backend/src/main/java/org/example/backend/service/order/OrderServiceImpl.java
@@ -30,6 +30,7 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public HttpEntity<?> handlePostOrder(OrderDto orderDto) {
+        if (orderDto.orderFoodDto().isEmpty()) throw new RuntimeException("Order food is empty");
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String name = authentication.getName();
         User user = userRepo.findByUsername(name).orElseThrow();
